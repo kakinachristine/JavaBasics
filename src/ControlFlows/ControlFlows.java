7fe6af60dca1c984027fae08065d404e3093f5db
kiki <christinekakina@gmail.com>
2024-08-12 11:41:35 +0300

    Control flows

---
+++ src/ControlFlows/ControlFlows.java
@@ -0,0 +1,78 @@
+//JAVA SELECTION STATEMENTS
+
+//IF STATEMENT : used to decide whether a certain statement or block of statement will be executed
+//                 or not.Executes iff condition is true
+            //     Syntax: if (condition){
+            //                 (statement)
+            //                }
+
+
+//IF-ELSE :gives an option if the statement is false
+//                 Syntax: if (condition){
+        //                 (statement for true condition)
+        //                }else{
+        //                 (statement for false condition)
+        //                }
+
+
+//NESTED IF  :control  statements inside other if statements
+//                 Syntax: if (condition){
+
+                    //         if (condition){
+                    //               (statement)
+                    //            }
+
+        //                }else{
+        //                 (statement for false condition)
+        //                }
+
+
+//IF-ELSE-IF :Passes the true condition , when false goes to the next statement and contains a default condition
+//                 Syntax: if (condition){
+        //                 (statement for true condition)
+        //                }else if (condition){
+        //                 (statement for false condition)
+        //                }
+        //                 .
+        //                 .
+        //                 .
+        //                }else{
+        //                 (statement for false condition)
+        //                }
+
+
+//Switch case : This is a multiway branch statement. It gives an easy way to dispatch execution to
+//              different parts of code based on the expression value
+//                 Syntax: switch (expression){
+             //              Case value1:
+             //                 Statement1;
+             //                 break;
+
+             //              Case value2:
+             //                 Statement2;
+             //                 break;
+                //                 .
+                //                 .
+                //                 .
+
+             //              default:
+             //                 StatementDefault;
+
+
+//Jump:Supports three jump statements:
+        //Break - Terminate sequence/ exit a loop
+        //Continue - Continue running the loop but stop processing the remainder of the code
+                    // in its body for this iteration (this skips out)
+        //Return - Used to explicitly return from a method
+
+
+
+
+
+package ControlFlows;
+
+public class ControlFlows {
+
+
+
+}
---
+++ src/ControlFlows/ForLoop.java
@@ -0,0 +1,10 @@
+package ControlFlows;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        // Increment after
+        for (int i = 0; i < 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
