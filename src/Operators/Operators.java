f33889a4ed963283d7c346937812339ad2697302
kiki <christinekakina@gmail.com>
2024-08-12 13:33:45 +0300

    Operators

--- src/Operators/Operators.java
+++ src/Operators/Operators.java
@@ -1,3 +1,42 @@
+//JAVA OPERATORS:
+
+//These are special symbols used to perform operations on variables and values.
+//They include:
+//              Arithmetic Operators:Perform basic arithmetic operations.
+//                                  + (Addition)
+//                                  - (Subtraction)
+//                                  * (Multiplication)
+//                                  / (Division)
+//                                  % (Modulus)
+//              Relational Operators:Compare two values.
+//                                  == (Equal to)
+//                                  != (Not equal to)
+//                                  > (Greater than)
+//                                  < (Less than)
+//                                  >= (Greater than or equal to)
+//                                  <= (Less than or equal to)
+//              Logical Operators:Perform logical operations.
+//                                && (Logical AND)
+//                                || (Logical OR)
+//                                 ! (Logical NOT)
+//              Assignment Operators:Assign values to variables.
+//                                  = (Simple assignment)
+//                                  +=, -=, *=, /=, %= (Compound assignment)
+//              Unary Operators:Perform operations on a single operand.
+//                              ++ (Increment)
+//                              -- (Decrement)
+//                              + (Unary plus)
+//                              - (Unary minus)
+//              Ternary Operators:A shorthand for if-else conditions( ? :)
+//              Ternary Operators:Perform bit-level operations.
+//                                & (Bitwise AND)
+//                                | (Bitwise OR)
+//                                ^ (Bitwise XOR)
+//                                ~ (Bitwise NOT)
+//                                <<, >>, >>> (Shift operators)
+
+
+
 package Operators;
 
 public class Operators {
